0ad3e2368811e07c7a29f82398ee28dcfe0a2d8d
Bryan Brophy <brybrophy@gmail.com>
2016-07-08 11:16:36 -0700

    Move logo on small screens

--- js/register.js
+++ js/register.js
@@ -4,11 +4,11 @@
   });
 
 
-const states = ['AL', 'AK', 'AZ', 'AR', 'CA',	'CO',	'CT',	'DE',	'FL',	'GA',	'HI',	'ID',	'IL',	'IN',	'IA',	'KS',	'KY',	'LA',	'ME',	'MD',	'MA',	'MI',	'MN',	'MS',	'MO',	'MT',	'NE',	'NV',	'NH',	'NJ',	'NM',	'NY',	'NC',	'ND',	'OH',	'OK',	'OR',	'PA',	'RI',	'SC',	'SD',	'TN',	'TX',	'UT',	'VT',	'VA',	'WA',	'WV',	'WI',	'WY']
+var states = ['AL', 'AK', 'AZ', 'AR', 'CA',	'CO',	'CT',	'DE',	'FL',	'GA',	'HI',	'ID',	'IL',	'IN',	'IA',	'KS',	'KY',	'LA',	'ME',	'MD',	'MA',	'MI',	'MN',	'MS',	'MO',	'MT',	'NE',	'NV',	'NH',	'NJ',	'NM',	'NY',	'NC',	'ND',	'OH',	'OK',	'OR',	'PA',	'RI',	'SC',	'SD',	'TN',	'TX',	'UT',	'VT',	'VA',	'WA',	'WV',	'WI',	'WY']
 
 $('.modal-trigger').click(function() {
-  let i = 1;
-  for (const state of states) {
+  var i = 1;
+  for (var state of states) {
     $('#states').append('<option value="' + i + '">' + state + '</option>');
     i += 1;
   }
