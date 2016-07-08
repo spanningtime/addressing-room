10eee7ec86247863fe6308f25ff6ad5727a11d0a
Bryan Brophy <brybrophy@gmail.com>
2016-07-08 10:15:28 -0700

    Finish building modal for registration

--- js/register.js
+++ js/register.js
@@ -1,6 +1,16 @@
-$(document).ready(function(){
-    $('.modal-trigger').leanModal({
-      dismissible: true,
-      opacity: .2
-    });
+  $('.modal-trigger').leanModal({
+    dismissible: true,
+    opacity: .2
   });
+
+
+const states = ['AL', 'AK', 'AZ', 'AR', 'CA',	'CO',	'CT',	'DE',	'FL',	'GA',	'HI',	'ID',	'IL',	'IN',	'IA',	'KS',	'KY',	'LA',	'ME',	'MD',	'MA',	'MI',	'MN',	'MS',	'MO',	'MT',	'NE',	'NV',	'NH',	'NJ',	'NM',	'NY',	'NC',	'ND',	'OH',	'OK',	'OR',	'PA',	'RI',	'SC',	'SD',	'TN',	'TX',	'UT',	'VT',	'VA',	'WA',	'WV',	'WI',	'WY']
+
+$('.modal-trigger').click(function() {
+  let i = 1;
+  for (const state of states) {
+    $('#states').append('<option value="' + i + '">' + state + '</option>');
+    i += 1;
+  }
+  $('select').material_select();
+});
