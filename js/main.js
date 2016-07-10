3dd7aef1bd88fdb037f4edf46bb9aecf5d1b1dac
Bryan Brophy <brybrophy@gmail.com>
2016-07-10 14:59:09 -0700

    Close #35 Add header bg-image and logo to welcome page

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@ $('.dropdown-button').dropdown({
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
       hover: false, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: true, // Displays dropdown below the button
-      alignment: 'left' // Displays dropdown with edge aligned to the left of button
+      gutter: 20, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
     }
 );
 
