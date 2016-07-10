e9d6c017dfcb845358a32b33048a4fc188c46b99
William Knight <kniwilliam@gmail.com>
2016-07-09 18:09:53 -0700

    Remove unnecessary notes for welcome.js

--- js/welcome.js
+++ js/welcome.js
@@ -4,12 +4,6 @@
 var recs = ['Bank of America', 'Amazon', 'Comcast', 'Seattle City Light', 'Ebay', 'Seattle Times', 'AT&T', 'University of Washington', 'Allstate', 'BECU', 'Etsy', 'Geico', 'Wells Fargo', 'Key Bank', 'US Bank'];
 var i = 1;
 
-var $bankOfAmerica = $('#bankOfAmerica');
-
-var recObject = {
-
-}
-
 $(document).ready(function() {
   for (var rec of recs) {
     $('#recs').append('<option class="recInput" value="' + i + '">' + rec + '</>')
@@ -18,12 +12,7 @@ $(document).ready(function() {
   $('select').material_select();
 });
 
-// $('li span input:first-child').eq(1).attr('checked', true)
-// When bank of america image is clicked on
-// add checkbox to dropdown list
 $('img').click(function(event) {
-  var clickArray = []
   $(event.target).toggleClass("select-border");
-  console.log(this.id);
   $('li span input:first-child').eq(this.id).attr('checked', true);
 });
