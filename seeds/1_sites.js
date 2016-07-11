7bc1cce4f7f81f444ed6a737b61cab0aed97868b
Bryan Brophy <brybrophy@gmail.com>
2016-07-11 12:00:39 -0700

    Fix my mistakes

--- seeds/1_sites.js
+++ seeds/1_sites.js
@@ -1,13 +1,13 @@
 
 exports.seed = function(knex) {
   return knex('sites').del()
-  .then(( => {
+  .then(() => {
     return knex('sites').insert([{
       id: 1,
       website_name: 'Amazon',
       url: 'www.amazon.com',
       instructions: 'Login to your user account. Click on "Your Account" in the navigation bar. Find the address book section and click "Manage Address" Book. Edit your old address or add a new one. Give a high five! You just changed your address at Amazon.',
-      img_url: '../images/sites_icons/amazon_icon.png'
+      img_url: '../images/sites_icons/amazon_icon.png',
       is_upToDate: false,
       created_at: new Date('2016-06-29 14:26:16 UTC'),
       updated_at: new Date('2016-06-29 14:26:16 UTC')
@@ -15,7 +15,7 @@ exports.seed = function(knex) {
   })
   .then(() => {
     return knex.raw(
-      "SELECT setval ('sites_id_seq', (SELECT MAX(id) FROM users))"
+      "SELECT setval ('sites_id_seq', (SELECT MAX(id) FROM sites))"
     );
   });
 };
--- seeds/2_memberships.js
+++ seeds/2_memberships.js
@@ -6,15 +6,15 @@ exports.seed = function(knex) {
       return knex('memberships')
         .insert([{
           id: 1,
-          book_id: 1,
           user_id: 1,
+          site_id: 1,
           created_at: new Date('2016-06-29 14:26:16 UTC'),
           updated_at: new Date('2016-06-29 14:26:16 UTC')
         }]);
     })
     .then(() => {
       return knex.raw(
-      "SELECT setval('memberships_id_seq', (SELECT MAX(id) FROM users_books));"
+      "SELECT setval('memberships_id_seq', (SELECT MAX(id) FROM memberships));"
       );
     });
 };
