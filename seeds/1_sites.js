b40179abc6ff06e211a03360bcf5e504619e939b
Bryan Brophy <brybrophy@gmail.com>
2016-07-11 12:48:46 -0700

    Move public files to public directory

--- seeds/1_sites.js
+++ seeds/1_sites.js
@@ -7,7 +7,7 @@ exports.seed = function(knex) {
       website_name: 'Amazon',
       url: 'www.amazon.com',
       instructions: 'Login to your user account. Click on "Your Account" in the navigation bar. Find the address book section and click "Manage Address" Book. Edit your old address or add a new one. Give a high five! You just changed your address at Amazon.',
-      img_url: '../images/sites_icons/amazon_icon.png',
+      img_url: '../public/images/sites_icons/amazon_icon.png',
       is_upToDate: false,
       created_at: new Date('2016-06-29 14:26:16 UTC'),
       updated_at: new Date('2016-06-29 14:26:16 UTC')
