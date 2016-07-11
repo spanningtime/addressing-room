9226cd1b9ee5571612e02c87acc55ded8ceb6143
William Knight <kniwilliam@gmail.com>
2016-07-11 11:34:49 -0700

    Close #52 Write seed for users

--- seeds/0_users.js
+++ seeds/0_users.js
@@ -1,13 +1,25 @@
+'use strict';
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      return Promise.all([
-        // Inserts seed entries
-        knex('table_name').insert({id: 1, colName: 'rowValue1'}),
-        knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-        knex('table_name').insert({id: 3, colName: 'rowValue3'})
-      ]);
-    });
+exports.seed = function(knex) {
+  return knex('users').del()
+  .then(() => {
+    return knex('users').insert([{
+      id: 1,
+      email: 'stanley@assbook.com'
+      hashed_password: '$2a$12$C9AYYmcLVGYlGoO4vSZTPud9ArJwbGRsJ6TUsNULzR48z8fOnTXbS',
+      name: 'Stanley Whatever',
+      address_1: '123 Bark Lane',
+      address_2: 'Apt 69',
+      city: 'Shoreline',
+      state: 'WA',
+      zip: '98155',
+      created_at: new Date('2016-06-29 14:26:16 UTC'),
+      updated_at: new Date('2016-06-29 14:26:16 UTC')
+    }])
+  })
+  .then(() => {
+    return knex.raw(
+      "SELECT setval ('users_id_seq', (SELECT MAX(id) FROM users))"
+    );
+  });
 };
