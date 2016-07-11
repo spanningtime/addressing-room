820aa94fb541a035854a408f53f6b5263208a5f3
Bryan Brophy <brybrophy@gmail.com>
2016-07-11 10:16:18 -0700

    Start server file

---
+++ server.js
@@ -0,0 +1,54 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+if (process.env.NODE_ENV !== 'test') {
+  const morgan = require('morgan');
+  app.use(morgan('short'));
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'bookshelf',
+  secret: process.env.SESSION_SECRET
+}));
+
+app.use(express.static(path.join('public')));
+
+app.use((_req, res) => {
+  res.sendStatus(404);
+});
+
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status) {
+    return res.status(err.status).send(err);
+  }
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
