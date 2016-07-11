42c101fc71da0a320fe1b30ee26ed12c025e0668
William Knight <kniwilliam@gmail.com>
2016-07-11 11:04:12 -0700

    Close #49 Write migration for users table

--- migrations/20160711103846_users.js
+++ migrations/20160711103846_users.js
@@ -1,8 +1,43 @@
+'use strict';
 
-exports.up = function(knex, Promise) {
-  
+exports.up = function(knex) {
+  return knex.schema.createTable('users', (table) => {
+    table
+      .increments();
+    table
+      .string('email')
+      .unique()
+      .notNullable();
+    table
+      .specificType('hashed_password', 'char(60)')
+      .notNullable();
+    table
+      .string('name')
+      .notNullable()
+      .defaultTo('');
+    table
+      .text('address_1')
+      .notNullable()
+      .defaultTo('');
+    table.text('address_2')
+      .notNullable()
+      .defaultTo('');
+    table
+      .string('city')
+      .notNullable()
+      .defaultTo('');
+    table
+      .specificType('state', 'char(2)')
+      .notNullable()
+      .defaultTo('');
+    table
+      .string('zip')
+      .notNullable()
+      .defaultTo('')
+    table.timestamps(true, true);
+  })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function(knex) {
+  return knex.schema.dropTable('users')
 };
