2d3e269435aecaed5050e6f02767df0097237b55
Bryan Brophy <brybrophy@gmail.com>
2016-07-11 11:00:29 -0700

    Close #50 Write migration for memberships table

--- migrations/20160711104000_memberships.js
+++ migrations/20160711104000_memberships.js
@@ -3,12 +3,22 @@
 exports.up = function(knex) {
   return knex.schema.createTable('memberships', (table) => {
     table.increments();
-    table.integer('memberships')
+    table.integer('user_id')
       .notNullable()
       .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .index();
+    table.integer('site_id')
+      .notNullable()
+      .references('id')
+      .inTable('sites')
+      .onDelete('CASCADE')
+      .index();
+    table.timestamps(true, true);
   });
 };
 
 exports.down = function(knex) {
-
+  return knex.schema.dropTable('memberships');
 };
