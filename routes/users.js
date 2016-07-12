2fe8bbf2986bf6a0887d0e7e76fc1ac0aa7599a2
Bryan Brophy <brybrophy@gmail.com>
2016-07-12 08:12:08 -0700

    Close #9 Build registration conformation email with mailgun-js

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,11 @@ const bcrypt = require('bcrypt-as-promised');
 const ev = require('express-validation');
 const validations = require('../validations/users');
 
+const Mailgun = require('mailgun-js');
+const api_key = 'key-f740d2cbc40cb72e3c00dc229d3ee613';
+const domain = 'www.bryanbrophy.com';
+const from_who = 'get@bryanbrophy.com';
+
 router.post('/users', ev(validations.post), (req, res, next) => {
   const { email, password, name, address_1, address_2, city, state, zip } = req.body;
 
@@ -33,6 +38,23 @@ router.post('/users', ev(validations.post), (req, res, next) => {
       });
     })
     .then(() => {
+      const mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+      const data = {
+        from: from_who,
+        to: email,
+        subject: 'Welcome to Addressing Room',
+        text: `Hey, ${name}!\n\nThank you for registering for Addressing Room! You can now add all the websites that you are a member of that keep track of your address to your use profile.\n\nDon't worry if you can't remember them all off the top of your head. We'll help you remember them all and we'll help you keep them all up to date!`
+      };
+
+      mailgun.messages().send(data, function (err, body) {
+        if (err) {
+          throw err;
+        }
+
+        console.log(body);
+      });
+
       res.sendStatus(200);
     })
     .catch((err) => {
