886a71038a3517dd344a28650b7451ccf1d807fb
William Knight <kniwilliam@gmail.com>
2016-07-13 16:05:18 -0700

    Close #87 Make all pages stylisticaly consistent

--- public/js/main.js
+++ public/js/main.js
@@ -15,9 +15,9 @@
 
   var userName = window.COOKIES.userName;
 
-  $('.nav-wrapper').prepend('<h4 class="brand-logo left user hide-on-med-and-down">HELLO ' + userName.toUpperCase() + '</h4>');
+  $('.nav-wrapper').prepend('<h4 class="brand-logo left user hide-on-med-and-down hello">HELLO ' + userName.toUpperCase() + '</h4>');
 
-  $('.medSmall').prepend('<h4 class="brand-logo center user userSm hide-on-large-only">HELLO ' + userName.toUpperCase() + '</h4>');
+  $('.medSmall').prepend('<h4 class="brand-logo center user userSm hide-on-large-only hello">HELLO ' + userName.toUpperCase() + '</h4>');
 
   $('.dropdown-button').dropdown({
     inDuration: 300,
