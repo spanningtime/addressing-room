ef34eb6aa22c197a9fe88101cf2ccc33e6215e3b
Bryan Brophy <brybrophy@gmail.com>
2016-07-17 15:28:36 -0700

    lint memberships seed

--- seeds/2_memberships.js
+++ seeds/2_memberships.js
@@ -1,49 +1,47 @@
-'use strict';
+(function() {
+  'use strict';
 
-exports.seed = function(knex) {
-  return knex('memberships').del()
-    .then(() => {
-      return knex('memberships')
-        .insert([{
-          id: 1,
-          user_id: 1,
-          site_id: 1,
-        },
-        {
-          id: 2,
-          user_id: 1,
-          site_id: 2,
-        },
-        {
-          id: 3,
-          user_id: 1,
-          site_id: 3,
-        },
-        {
-          id: 4,
-          user_id: 1,
-          site_id: 4,
-        },
-        {
-          id: 5,
-          user_id: 2,
-          site_id: 2,
-        },
-        {
-          id: 6,
-          user_id: 2,
-          site_id: 5,
-        },
-        {
-          id: 7,
-          user_id: 2,
-          site_id: 10,
-        },
-      ])
-    })
-    .then(() => {
-      return knex.raw(
+  exports.seed = function(knex) {
+    return knex('memberships').del()
+    .then(() => knex('memberships')
+      .insert([{
+        id: 1,
+        user_id: 1,
+        site_id: 1
+      },
+      {
+        id: 2,
+        user_id: 1,
+        site_id: 2
+      },
+      {
+        id: 3,
+        user_id: 1,
+        site_id: 3
+      },
+      {
+        id: 4,
+        user_id: 1,
+        site_id: 4
+      },
+      {
+        id: 5,
+        user_id: 2,
+        site_id: 2
+      },
+      {
+        id: 6,
+        user_id: 2,
+        site_id: 5
+      },
+      {
+        id: 7,
+        user_id: 2,
+        site_id: 10
+      }
+  ]))
+  .then(() => knex.raw(
       "SELECT setval('memberships_id_seq', (SELECT MAX(id) FROM memberships));"
-      );
-    });
-};
+  ));
+  };
+})();
