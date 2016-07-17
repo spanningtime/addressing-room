42893b0c2f3b5ad349001911fab7dbe659bb7930
Bryan Brophy <brybrophy@gmail.com>
2016-07-17 15:31:05 -0700

    lint users migration

--- migrations/20160711103846_users.js
+++ migrations/20160711103846_users.js
@@ -1,35 +1,37 @@
-'use strict';
+(function() {
+  'use strict';
 
-exports.up = function(knex) {
-  return knex.schema.createTable('users', (table) => {
-    table.increments();
-    table.string('email')
+  exports.up = function(knex) {
+    return knex.schema.createTable('users', (table) => {
+      table.increments();
+      table.string('email')
       .unique()
       .notNullable();
-    table.specificType('hashed_password', 'char(60)')
+      table.specificType('hashed_password', 'char(60)')
       .notNullable();
-    table.string('name')
+      table.string('name')
       .notNullable()
       .defaultTo('');
-    table.text('address_1')
+      table.text('address_1')
       .notNullable()
       .defaultTo('');
-    table.text('address_2')
+      table.text('address_2')
       .notNullable()
       .defaultTo('');
-    table.string('city')
+      table.string('city')
       .notNullable()
       .defaultTo('');
-    table.string('state')
+      table.string('state')
       .notNullable()
       .defaultTo('');
-    table.string('zip')
+      table.string('zip')
       .notNullable()
-      .defaultTo('')
-    table.timestamps(true, true);
-  })
-};
+      .defaultTo('');
+      table.timestamps(true, true);
+    });
+  };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('users')
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('users');
+  };
+})();
