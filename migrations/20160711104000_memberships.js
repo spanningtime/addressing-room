c625a0a0703728e8935470388652f9c822b23354
Bryan Brophy <brybrophy@gmail.com>
2016-07-17 15:34:49 -0700

    lint memberships migration

--- migrations/20160711104000_memberships.js
+++ migrations/20160711104000_memberships.js
@@ -1,27 +1,29 @@
-'use strict';
+(function() {
+  'use strict';
 
-exports.up = function(knex) {
-  return knex.schema.createTable('memberships', (table) => {
-    table.increments();
-    table.integer('user_id')
+  exports.up = function(knex) {
+    return knex.schema.createTable('memberships', (table) => {
+      table.increments();
+      table.integer('user_id')
       .notNullable()
       .references('id')
       .inTable('users')
       .onDelete('CASCADE')
       .index();
-    table.integer('site_id')
+      table.integer('site_id')
       .notNullable()
       .references('id')
       .inTable('sites')
       .onDelete('CASCADE')
       .index();
-    table.boolean('is_upToDate')
+      table.boolean('is_upToDate')
       .notNullable()
       .defaultTo(false);
-    table.timestamps(true, true);
-  });
-};
+      table.timestamps(true, true);
+    });
+  };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('memberships');
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('memberships');
+  };
+})();
