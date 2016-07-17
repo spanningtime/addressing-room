e363ae289db0493d80321c8c1280d912a5e3d035
Bryan Brophy <brybrophy@gmail.com>
2016-07-17 14:45:23 -0700

    lint users and memberships route

--- routes/memberships.js
+++ routes/memberships.js
@@ -1,44 +1,44 @@
-'use strict';
+(function() {
+  'use strict';
 
-const express = require('express');
-const router = express.Router();
-const knex = require('../knex');
+  const express = require('express');
+  const router = express.Router();
+  const knex = require('../knex');
 
-const ev = require('express-validation');
-const validations = require('../validations/memberships');
+  const checkAuth = function(req, res, next) {
+    if (!req.session.userId) {
+      const err = new Error('Unauthorized');
 
-const checkAuth = function(req, res, next) {
-  if (!req.session.userId) {
-    const err = new Error('Unauthorized');
-    err.statusCode = 401;
+      err.statusCode = 401;
 
-    return next(err);
-  }
+      return next(err);
+    }
 
-  next();
-};
+    next();
+  };
 
-router.post('/memberships', checkAuth, (req, res, next) => {
-  const user_id = req.body.user_id;
-  const website_id = req.body.website_id;
+  router.post('/memberships', checkAuth, (req, res, next) => {
+    const user_id = req.body.user_id;
+    const website_id = req.body.website_id;
 
-  knex('memberships')
+    knex('memberships')
     .where('user_id', user_id)
     .where('site_id', website_id)
     .first()
     .then((membership) => {
       if (membership) {
         const err = new Error('Duplicate membership');
+
         err.status = 400;
 
         throw err;
       }
 
       return knex('memberships')
-        .insert({
-          user_id: user_id,
-          site_id: website_id
-        }, '*');
+      .insert({
+        user_id,
+        site_id: website_id
+      }, '*');
     })
     .then((memberships) => {
       res.send(memberships[0]);
@@ -46,93 +46,93 @@ router.post('/memberships', checkAuth, (req, res, next) => {
     .catch((err) => {
       next(err);
     });
-});
+  });
 
-router.patch('/memberships', (req, res, next) => {
-  const user_id = req.body.user_id;
-  const website_id = req.body.website_id;
+  router.patch('/memberships', (req, res, next) => {
+    const user_id = req.body.user_id;
+    const website_id = req.body.website_id;
 
-  knex('memberships')
+    knex('memberships')
     .where('user_id', user_id)
     .where('site_id', website_id)
-    .update({ is_upToDate: 't'})
+    .update({ is_upToDate: 't' })
     .then((memberships) => {
       res.send(memberships[0]);
     })
     .catch((err) => {
       next(err);
     });
-});
+  });
 
-router.get('/memberships', checkAuth, (req, res, next) => {
-  const user_id = req.session.userId;
+  router.get('/memberships', checkAuth, (req, res, next) => {
+    const userId = req.session.userId;
 
-  console.log(user_id);
-
-  knex('sites')
+    knex('sites')
     .orderBy('sites.website_name')
     .innerJoin('memberships', 'memberships.site_id', 'sites.id')
-    .where('memberships.user_id', req.session.userId)
+    .where('memberships.user_id', userId)
     .then((sites) => {
       res.send(sites);
     })
     .catch((err) => {
       next(err);
     });
-});
-
-router.get('/memberships/:siteId', checkAuth, (req, res, next) => {
-  const user_id = req.session.userId;
-  const website_id = Number.parseInt(req.params.siteId);
-
-  knex('sites')
-  .innerJoin('memberships', 'memberships.site_id', 'sites.id')
-  .where({
-    'sites.id': website_id,
-    'memberships.user_id': user_id
-  })
-  .first()
-  .then((site) => {
-    if(!site) {
-      return next();
-    }
-
-    res.send(site);
-  })
-  .catch((err) => {
-    next(err);
   });
-});
 
-router.delete('/memberships/:siteId', checkAuth, (req, res, next) => {
-  const user_id = req.session.userId;
-  const website_id = Number.parseInt(req.params.siteId);
+  router.get('/memberships/:siteId', checkAuth, (req, res, next) => {
+    const user_id = req.session.userId;
+    const website_id = Number.parseInt(req.params.siteId);
 
-  knex('memberships')
+    knex('sites')
+    .innerJoin('memberships', 'memberships.site_id', 'sites.id')
+    .where({
+      'sites.id': website_id,
+      'memberships.user_id': user_id
+    })
+    .first()
+    .then((site) => {
+      if (!site) {
+        return next();
+      }
+
+      res.send(site);
+    })
+    .catch((err) => {
+      next(err);
+    });
+  });
+
+  router.delete('/memberships/:siteId', checkAuth, (req, res, next) => {
+    const user_id = req.session.userId;
+    const website_id = Number.parseInt(req.params.siteId);
+
+    knex('memberships')
     .where({
       'memberships.user_id': user_id,
       'memberships.site_id': website_id
     })
     .first()
     .then((site) => {
-      if(!site) {
+      if (!site) {
         return next();
       }
 
       return knex('memberships')
-        .del()
-        .where({
-          'memberships.user_id': user_id,
-          'memberships.site_id': website_id
-        })
-        .then(() => {
-          delete site.id;
-          return res.send(site);
-        });
+      .del()
+      .where({
+        'memberships.user_id': user_id,
+        'memberships.site_id': website_id
+      })
+      .then(() => {
+        delete site.id;
+
+        return res.send(site);
+      });
     })
     .catch((err) => {
       next(err);
     });
-});
+  });
 
-module.exports = router;
+  module.exports = router;
+})();
--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,62 @@
-'use strict';
+(function() {
+  'use strict';
 
-const express = require('express');
-const router = express.Router();
+  const express = require('express');
+  const router = express.Router();
 
-const knex = require('../knex');
-const bcrypt = require('bcrypt-as-promised');
+  const knex = require('../knex');
+  const bcrypt = require('bcrypt-as-promised');
 
-const ev = require('express-validation');
-const validations = require('../validations/users');
+  const ev = require('express-validation');
+  const validations = require('../validations/users');
 
-const Mailgun = require('mailgun-js');
-const api_key = 'key-f740d2cbc40cb72e3c00dc229d3ee613';
-const domain = 'www.bryanbrophy.com';
-const from_who = 'get@bryanbrophy.com';
+  const Mailgun = require('mailgun-js');
+  const apiKey = 'key-f740d2cbc40cb72e3c00dc229d3ee613';
+  const domain = 'www.bryanbrophy.com';
+  const fromWho = 'get@bryanbrophy.com';
 
-router.post('/users', ev(validations.post), (req, res, next) => {
-  const { email, password, name, address_1, address_2, city, state, zip } = req.body;
+  router.post('/users', ev(validations.post), (req, res, next) => {
+    const {
+      email,
+      password,
+      name,
+      address_1,
+      address_2,
+      city,
+      state,
+      zip } = req.body;
 
-  knex('users')
+    knex('users')
     .select(knex.raw('1=1'))
     .where('email', email)
     .first()
     .then((exists) => {
       if (exists) {
         return res
-          .status(409)
-          .set('Content-Type', 'text/plain')
-          .send('Email already exists, Motherfucker.')
+        .status(409)
+        .set('Content-Type', 'text/plain')
+        .send('Email already exists.');
       }
 
       return bcrypt.hash(password, 12);
     })
-    .then((hashed_password) => {
-      return knex('users').insert({
-        email, hashed_password, name, address_1, address_2, city, state, zip
-      }, '*');
-    })
+    .then((hashed_password) => knex('users').insert({
+      email, hashed_password, name, address_1, address_2, city, state, zip
+    }, '*')
+    )
     .then((usersInserted) => {
-      const mailgun = new Mailgun({apiKey: api_key, domain: domain});
+      const mailgun = new Mailgun({ apiKey, domain });
 
       const data = {
-        from: from_who,
+        from: fromWho,
         to: email,
         subject: 'Welcome to Addressing Room',
-        text: `Hey, ${name}!\n\nThank you for registering for Addressing Room! You are a true soldier, and we salute you, dawg.\n\nHugs and Kisses...\nLove, Ya Boiyz at Addressing Room HQ (member FDIC)`
+        text: `Hey, ${name}!\n\nThank you for registering for Addressing Room!
+        You are a true soldier, and we salute you, dawg.\n\nHugs and
+        Kisses...\nLove, Ya Boiyz at Addressing Room HQ (member FDIC)`
       };
 
-      mailgun.messages().send(data, (err, body) => {
+      mailgun.messages().send(data, (err, _body) => {
         if (err) {
           throw err;
         }
@@ -61,19 +71,20 @@ router.post('/users', ev(validations.post), (req, res, next) => {
     .catch((err) => {
       next(err);
     });
-});
+  });
 
-router.patch('/users/:id', (req, res, next) => {
-  knex('users')
+  router.patch('/users/:id', (req, res, next) => {
+    knex('users')
     .update(req.body, '*')
     .where('id', req.params.id)
     .then((users) => {
       res.cookie('userName', users[0].name, { encode: String });
-      res.send(users[0])
+      res.send(users[0]);
     })
     .catch((err) => {
       next(err);
     });
-});
+  });
 
-module.exports = router;
+  module.exports = router;
+})();
