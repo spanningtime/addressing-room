1540abbc30cd5014c40d70fca105ee445260acea
Bryan Brophy <brybrophy@gmail.com>
2016-07-17 15:33:49 -0700

    lint sites migration

--- migrations/20160711103943_sites.js
+++ migrations/20160711103943_sites.js
@@ -1,23 +1,26 @@
+(function() {
+  'use strict';
 
-exports.up = function(knex) {
-  return knex.schema.createTable('sites', (table) => {
-    table.increments();
-    table.string('website_name')
-      .notNullable()
-      .defaultTo('')
-    table.string('url')
+  exports.up = function(knex) {
+    return knex.schema.createTable('sites', (table) => {
+      table.increments();
+      table.string('website_name')
       .notNullable()
       .defaultTo('');
-    table.text('instructions')
+      table.string('url')
+      .notNullable()
+      .defaultTo('');
+      table.text('instructions')
       .notNullable()
       .defaultTo('Hey! It looks like you added this site on your own. We are working on finding instructions to help you in the future.');
-    table.string('img_url')
+      table.string('img_url')
       .notNullable()
       .defaultTo('');
-    table.timestamps(true, true);
-  })
-};
+      table.timestamps(true, true);
+    });
+  };
 
-exports.down = function(knex) {
-  return knex.schema.dropTable('sites');
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTable('sites');
+  };
+})();
